5e7aa5e9d8720ef9ca3b68990bcf814cbd7c2ad3
unknown <backtv020@gmail.com>
2026-02-06 18:37:00 -0300

    Limpeza: Removendo arquivos temporários e banco de dados

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,3 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    botao_confirmar = SubmitField('Fazer Login')
+
+class FormCriarConta(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmar_senha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo()])
